3116fa79ecd3c5df0f985960ec5cbd156b4594f1
Tim <harvard90873@gmail.com>
2022-01-04 18:24:45 +0100

    more conversions

--- converter.py
+++ converter.py
@@ -5,6 +5,24 @@ def bitsToString(bits):
     # join the bits to form a string
     return "".join(map(lambda x: str(x), bits))
 
+def signed_to_twos_complement(signed_bits: List[int]) -> List[int]:
+    # 1. flip all the digits
+    for i in range(len(signed_bits)):
+        if signed_bits[i] == 1:
+            signed_bits[i] = 0
+        elif signed_bits[i] == 0:
+            signed_bits[i] = 1
+
+    # 2. add one to the number
+    stringified = str(signed_bits)
+    with_one_added = bin(int(stringified, 2) + 1)
+    ## Removing the binary indicators in front eg. `0b1001` => `1001`
+    ## and converting the resulting string to a list of integers
+    bits = list(map(lambda x: int(x), list(with_one_added[2:])))
+
+    return bits
+
+
 def unsigned_to_denary(unsigned_bits: List[int]):
     base = 1
     res = 0
@@ -14,6 +32,17 @@ def unsigned_to_denary(unsigned_bits: List[int]):
         i -= 1
         base *= 2
     return res
+
+def signed_to_denary(signed_bits: List[int]):
+    # if the left most digit of the binary number
+    # is one, it's negative
+    if signed_bits[0] == 1:
+        twos_complement = signed_to_twos_complement(signed_bits)
+        return unsigned_to_denary(twos_complement) * -1
+    else:
+        # it's positive, just treat it like an unsigned binary number
+        # because an unsigned number is positive, and the most significant digit is zero(thus the calculation won't be affected)
+        return unsigned_to_denary(signed_bits)
 
 def denary_to_unsigned(denary: int):
     # reject negative values
